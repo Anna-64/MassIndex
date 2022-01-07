db8c984b8e81784d84b346d444f67a496dd4a08d
Anna-64 <shurygina6410@gmail.com>
2022-01-07 22:24:47 +0400

    Body commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
-    public double calculate(double computation) {
-        double height = 1.76 * 1.76;
-        double massIndex = computation / height;
+    public double calculate(double weight, double height) {
+        double heightSquared = Math.pow(height, 2);
+        double massIndex = weight / heightSquared;
         return massIndex;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double weight = 60;
-        double massIndex = service.calculate(weight);
-        System.out.println("Вес тела: " + weight);
+        double massIndex = service.calculate(60, 1.76);
+        double massIndex1 = service.calculate(62, 1.75);
         System.out.println("Индекс массы тела: " + massIndex);
+        System.out.println("Индекс массы тела: " + massIndex1);
     }
 }
 
